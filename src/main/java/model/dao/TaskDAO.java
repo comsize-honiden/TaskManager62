89a41b1cc99729a10163dbc42bb679dc16840071
comsize-yoshizawa <kou.yoshizawa@comsize.com>
2026-04-15 15:24:28 +0900

    新規作成 タスク一覧DAO等
    TaskDAO,TaskListServlet,task-list,jspを記述

--- src/main/java/model/dao/TaskDAO.java
+++ src/main/java/model/dao/TaskDAO.java
@@ -3,25 +3,40 @@ package model.dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entity.TaskBean;
 
 public class TaskDAO {
-	public <List>TaskBean getTaskList(TaskBean task) {
-		String sql = "SELECT * FROM t_task t1 INNER JOIN m_category t2 ON t1.category_id = t2.category_id";
-		<List>TaskBean result = new <List>TaskBean();
+	public List<TaskBean> getTaskList() throws ClassNotFoundException, SQLException {
+		String sql = "SELECT * FROM t_task";
+		List<TaskBean> result = new ArrayList<TaskBean>();
 
 		try (Connection con = ConnectionManager.getConnection();
 				Statement stmt = con.createStatement();
 				ResultSet rs = stmt.executeQuery(sql)) {
 			while(rs.next()) {
 				String taskName = rs.getString("task_name");
-				String categoryName = rs.getString("category_name");
+				int categoryId = rs.getInt("category_id");
 				Date limitDate = rs.getDate("limit_date");
 				String userId = rs.getString("user_id");
-				String statusName = rs.getString("status_name");
+				String statusCode = rs.getString("status_code");
+				String memo = rs.getString("memo");
 				
+				TaskBean task = new TaskBean();
+				
+				task.setTaskName(taskName);
+				task.setCategoryId(categoryId);
+				task.setLimitDate(limitDate);
+				task.setUserId(userId);
+				task.setStatusCode(statusCode);
+				task.setMemo(memo);
+				
+				result.add(task);
 			}
-			
 		}
 		return result;
 	}
--- src/main/java/servlet/TaskListServlet.java
+++ src/main/java/servlet/TaskListServlet.java
@@ -1,7 +1,10 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import model.dao.TaskDAO;
+import model.entity.TaskBean;
 
 /**
  * Servlet implementation class TaskListServlet
@@ -16,19 +20,20 @@ import model.dao.TaskDAO;
 @WebServlet("/task-list-servlet")
 public class TaskListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public TaskListServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public TaskListServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
@@ -36,12 +41,23 @@ public class TaskListServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		
-		TaskDAO dao = new TaskDAO();
-		
-		
-	}
 
+		TaskDAO dao = new TaskDAO();
+		try {
+			List<TaskBean> taskList = dao.getTaskList();
+			request.setAttribute("taskList", taskList);
+		} catch (ClassNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		RequestDispatcher rd = request.getRequestDispatcher("task-list.jsp");
+		rd.forward(request, response);
+	}
 }
