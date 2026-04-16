00319a4e3684d5feb403207da5a5eb0cfbf32376
comsize-yamamura <takafumi.yamamura@comsize.com>
2026-04-16 10:16:03 +0900

    編集　sampleDAO getTaskBeanListメソッドの追加

--- src/main/java/model/dao/SampleDAO.java
+++ src/main/java/model/dao/SampleDAO.java
@@ -4,11 +4,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import model.entity.CategoryBean;
 import model.entity.StatusBean;
+import model.entity.TaskBean;
 import model.entity.UserBean;
 
 public class SampleDAO {
@@ -104,6 +107,52 @@ public class SampleDAO {
 			
 			System.out.println(statusBeanList.size());
 			return statusBeanList;
+			
+		}
+		
+	}
+	
+	public List<TaskBean> getTaskBeanList() throws SQLException, ClassNotFoundException {
+		
+		List<TaskBean> taskBeanList = new ArrayList<>();
+		
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement
+					("SELECT * FROM m_task")) {
+			
+			ResultSet res = pstmt.executeQuery();
+			
+			while (res.next()) {
+			
+				TaskBean task = new TaskBean();
+				
+				int taskId = res.getInt("task_id");
+				String taskName = res.getNString("task_name");
+				int categoryId = res.getInt("category_id");
+				LocalDate limitDate = res.getObject("limit_date", LocalDate.class);
+				String userId = res.getString("user_id");
+				String statusCode = res.getString("stasus_code");
+				String memo = res.getString("memo");
+				LocalDateTime createDatetime = res.getObject("create_datetime", LocalDateTime.class);
+				LocalDateTime updateDatetime = res.getObject("update_datetime", LocalDateTime.class);
+				
+				task.setTaskId(taskId);
+				task.setTaskName(taskName);
+				task.setCategoryId(categoryId);
+				task.setLimitDate(limitDate);
+				task.setUserId(userId);
+				task.setStatusCode(statusCode);
+				task.setMemo(memo);
+				task.setCreateDatetime(createDatetime);
+				task.setUpdateDatetime(updateDatetime);
+				
+				
+				taskBeanList.add(task);
+				
+			}
+			
+			System.out.println(taskBeanList.size());
+			return taskBeanList;
 			
 		}
 		
