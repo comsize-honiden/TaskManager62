95d28f3ac2de333878c7a4e4aaf256dd47b2c4e3
comsize-yoshizawa <kou.yoshizawa@comsize.com>
2026-04-17 16:50:39 +0900

    追加
    CommentBean追加
    
    (cherry picked from commit 2cea8dbb2f61a5550e687f0cbde50a1490050230)

---
+++ src/main/java/model/entity/CommentBean.java
@@ -0,0 +1,52 @@
+package model.entity;
+
+import java.time.LocalDateTime;
+
+public class CommentBean {
+	private int commentId;
+	private int taskId;
+	private String userId;
+	private String comment;
+	private LocalDateTime updateDateTime;
+
+	public int getCommentId() {
+		return commentId;
+	}
+
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+
+	public int getTaskId() {
+		return taskId;
+	}
+
+	public void setTaskId(int taskId) {
+		this.taskId = taskId;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public LocalDateTime getUpdateDateTime() {
+		return updateDateTime;
+	}
+
+	public void setUpdateDateTime(LocalDateTime updateDateTime) {
+		this.updateDateTime = updateDateTime;
+	}
+
+}
