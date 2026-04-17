a91c0b6058642425c15ff1940a67b339f773781c
comsize-yamamura <takafumi.yamamura@comsize.com>
2026-04-17 14:40:10 +0900

    編集　TaskDAO
    タスク変更メソッドの追加

--- src/main/java/model/dao/TaskDAO.java
+++ src/main/java/model/dao/TaskDAO.java
@@ -3,28 +3,41 @@ package model.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 import model.entity.TaskBean;
 
 public class TaskDAO {
 
 	//タスクの変更を行うメソッド
-	public int updateTask(TaskBean task) throws SQLException, ClassNotFoundException {
+	public int taskAlter(TaskBean task) throws SQLException, ClassNotFoundException {
 		
 		try (Connection con = ConnectionManager.getConnection();
 				PreparedStatement pstmt = con.prepareStatement
-					("UPDATE t_task SET"
-						+ "task_name = ?"
-						+ "categoryId = ?"
-						+ "limitDate = ?"
-						+ "userId = ?"
-						+ "statusCode = ?"
-						+ "memo = ?"
-						+ "createDatetime = ?"
-						+ "updateDatetime = ?"
-						+ " WHERE task_id = ?")) {
+					("UPDATE t_task SET task_name = ?, category_id = ?, limit_date = ?,user_id = ?, status_code = ?, memo = ? WHERE task_id = ?;")) {
 			
+			int taskId = task.getTaskId();
+			String taskName = task.getTaskName();
+			int categoryId = task.getTaskId();
+			LocalDate limitDate = task.getLimitDate();
+			String limitDateStr = limitDate.toString();
+			String userId = task.getUserId();
+			String statusCode = task.getStatusCode();
+			String memo = task.getMemo();
 			
+			pstmt.setString(1, taskName);
+			pstmt.setInt(2, categoryId);
+			pstmt.setString(3, limitDateStr);
+			pstmt.setString(4, userId);
+			pstmt.setString(5, statusCode);
+			pstmt.setString(6, memo);
+			pstmt.setInt(7, taskId);
+
+			
+			int res = pstmt.executeUpdate();
+			
+			return res;
+				
 		}
 		
 	}
