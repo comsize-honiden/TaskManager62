5b387d11065f12a4de3f6f9ad4bad3fa77035f69
comsize-watanabe <minori.watanabe@comsize.com>
2026-04-21 10:52:53 +0900

    更新　SampleDAO
    各BeanListの名前の微調整

--- src/main/java/model/dao/SampleDAO.java
+++ src/main/java/model/dao/SampleDAO.java
@@ -16,9 +16,9 @@ import model.entity.UserBean;
 
 public class SampleDAO {
 
-	public List<UserBean> getUserBeanList() throws SQLException, ClassNotFoundException {
+	public List<UserBean> getUserList() throws SQLException, ClassNotFoundException {
 		
-		List<UserBean> userBeanList = new ArrayList<>();
+		List<UserBean> userList = new ArrayList<>();
 		
 		try (Connection con = ConnectionManager.getConnection();
 				PreparedStatement pstmt = con.prepareStatement
@@ -38,19 +38,19 @@ public class SampleDAO {
 				user.setPassword(pass);
 				user.setUserName(userName);
 				
-				userBeanList.add(user);
+				userList.add(user);
 				
 			}
 			
-			return userBeanList;
+			return userList;
 			
 		}
 		
 	}
 	
-	public List<CategoryBean> getCategoryBeanList() throws SQLException, ClassNotFoundException {
+	public List<CategoryBean> getCategoryList() throws SQLException, ClassNotFoundException {
 		
-		List<CategoryBean> categoryBeanList = new ArrayList<>();
+		List<CategoryBean> categoryList = new ArrayList<>();
 		
 		try (Connection con = ConnectionManager.getConnection();
 				PreparedStatement pstmt = con.prepareStatement
@@ -68,19 +68,19 @@ public class SampleDAO {
 				category.setCategoryId(categoryId);
 				category.setCategoryName(categoryName);
 				
-				categoryBeanList.add(category);
+				categoryList.add(category);
 				
 			}
 			
-			return categoryBeanList;
+			return categoryList;
 			
 		}
 		
 	}
 	
-	public List<StatusBean> getStatusBeanList() throws SQLException, ClassNotFoundException {
+	public List<StatusBean> getStatusList() throws SQLException, ClassNotFoundException {
 		
-		List<StatusBean> statusBeanList = new ArrayList<>();
+		List<StatusBean> statusList = new ArrayList<>();
 		
 		try (Connection con = ConnectionManager.getConnection();
 				PreparedStatement pstmt = con.prepareStatement
@@ -99,19 +99,19 @@ public class SampleDAO {
 				status.setStatusName(statusName);
 				
 				
-				statusBeanList.add(status);
+				statusList.add(status);
 				
 			}
 			
-			return statusBeanList;
+			return statusList;
 			
 		}
 		
 	}
 	
-	public List<TaskBean> getTaskBeanList() throws SQLException, ClassNotFoundException {
+	public List<TaskBean> getTaskList() throws SQLException, ClassNotFoundException {
 		
-		List<TaskBean> taskBeanList = new ArrayList<>();
+		List<TaskBean> taskList = new ArrayList<>();
 		
 		try (Connection con = ConnectionManager.getConnection();
 				PreparedStatement pstmt = con.prepareStatement
@@ -144,11 +144,11 @@ public class SampleDAO {
 				task.setUpdateDatetime(updateDatetime); 
 				
 				
-				taskBeanList.add(task);
+				taskList.add(task);
 				
 			}
 			
-			return taskBeanList;
+			return taskList;
 			
 		}
 		
