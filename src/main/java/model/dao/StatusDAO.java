9596f320302738ec06d8445326291a772b29f558
comsize-watanabe <minori.watanabe@comsize.com>
2026-04-22 10:58:34 +0900

    追加　StatusDAO
    チェリピ

---
+++ src/main/java/model/dao/StatusDAO.java
@@ -0,0 +1,31 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entity.StatusBean;
+
+public class StatusDAO {
+	public List<StatusBean> getStatusList() throws ClassNotFoundException, SQLException{
+		String sql = "SELECT * FROM m_category";
+		List<StatusBean> result = new ArrayList<StatusBean>();
+		
+		try (Connection con = ConnectionManager.getConnection();
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(sql)){
+			while(rs.next()) {
+				String StatusName = rs.getString("status_name");
+				StatusBean status = new StatusBean();
+				status.setStatusName(StatusName);
+				
+				result.add(status);
+			}
+		}
+		return result;
+	}
+}
+
