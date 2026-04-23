d0c81c3a141ab374fb3c321d97c72d1b328ad033
comsize-honiden <junya.honiden@comsize.com>
2026-04-23 17:55:27 +0900

    更新　TaskDAO.java
    完成(暫定)

--- src/main/java/model/dao/TaskDAO.java
+++ src/main/java/model/dao/TaskDAO.java
@@ -3,7 +3,6 @@ package model.dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 
@@ -18,11 +17,8 @@ public class TaskDAO {
 		
 		try (Connection con = ConnectionManager.getConnection();
 				PreparedStatement pstmt = con.prepareStatement
-					("INSERT INTO t_task (task_id, task_name, category_id, limit_date, user_id, status_code, memo) VALUES (?, ?, ?, ?, ?, ?, ?)")) {
+				("INSERT INTO t_task (task_name, category_id, limit_date, user_id, status_code, memo) VALUES (?, ?, ?, ?, ?, ?)")) {
 			
-			ResultSet res = pstmt.executeQuery();
-			
-			int taskId = task.getTaskId();
 			String taskName = task.getTaskName();
 			int categoryId = task.getCategoryId();
 			LocalDate limitDate = task.getLimitDate();
@@ -30,24 +26,22 @@ public class TaskDAO {
 			String statusCode = task.getStatusCode();
 			String memo = task.getMemo();
 			
-			Date sqlLimitDate = Date.valueOf(limitDate);
+			Date sqlLimitDate;
 			
-			if (taskName.length() <= 50
-				&& userId.length() <= 24
-				&& memo.length() <= 100) {
-			
-				pstmt.setInt(1, taskId);
-				pstmt.setString(2, taskName);
-				pstmt.setInt(3, categoryId);
-				pstmt.setDate(4, sqlLimitDate);
-				pstmt.setString(5, userId);
-				pstmt.setString(6, statusCode);
-				pstmt.setString(7, memo);
-				insertCount = pstmt.executeUpdate();
-			
+			if (limitDate == null) {
+				sqlLimitDate = null;
 			} else {
-				insertCount = 0;
+				sqlLimitDate = Date.valueOf(limitDate);
 			}
+			
+			pstmt.setString(1, taskName);
+			pstmt.setInt(2, categoryId);
+			pstmt.setDate(3, sqlLimitDate);
+			pstmt.setString(4, userId);
+			pstmt.setString(5, statusCode);
+			pstmt.setString(6, memo);
+				
+			insertCount = pstmt.executeUpdate();
 		}
 		
 		return insertCount;
