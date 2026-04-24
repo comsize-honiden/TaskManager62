c0824417764e9401dbf36980cbec560c21d3b8fd
comsize-honiden <junya.honiden@comsize.com>
2026-04-24 17:21:16 +0900

    修正　StatusDAO.java
    beanにステータスコードを設定する処理を追加しました

--- src/main/java/model/dao/StatusDAO.java
+++ src/main/java/model/dao/StatusDAO.java
@@ -11,20 +11,26 @@ import model.entity.StatusBean;
 
 public class StatusDAO {
 	public List<StatusBean> getStatusList() throws ClassNotFoundException, SQLException{
+		
 		String sql = "SELECT * FROM m_status";
+		
 		List<StatusBean> result = new ArrayList<StatusBean>();
 		
 		try (Connection con = ConnectionManager.getConnection();
 				Statement stmt = con.createStatement();
 				ResultSet rs = stmt.executeQuery(sql)){
+			
 			while(rs.next()) {
-				String StatusName = rs.getString("status_name");
+				String statusCode = rs.getString("status_code");
+				String statusName = rs.getString("status_name");
 				StatusBean status = new StatusBean();
-				status.setStatusName(StatusName);
+				status.setStatusCode(statusCode);
+				status.setStatusName(statusName);
 				
 				result.add(status);
 			}
 		}
+		
 		return result;
 	}
 }
