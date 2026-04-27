853433842fa785dac4d83d9431c8dd9848f9ecd4
comsize-watanabe <minori.watanabe@comsize.com>
2026-04-27 09:25:30 +0900

    更新　CommentPostServlet
    破壊耐性

--- src/main/java/servlet/CommentPostServlet.java
+++ src/main/java/servlet/CommentPostServlet.java
@@ -2,6 +2,7 @@ package servlet;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -35,8 +36,7 @@ public class CommentPostServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		RequestDispatcher rd = request.getRequestDispatcher("sample.jsp");
-		rd.forward(request, response);
+		doPost(request, response);
 	}
 
 	/**
@@ -47,39 +47,53 @@ public class CommentPostServlet extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		String commentText = request.getParameter("comment");
 		
-		/* 本番用のタスクIDとログインユーザー取得のための記述
+		/* 対象タスクの詳細とログインユーザーをセッションから取得する
 		HttpSession session = request.getSession();
 		TaskBean task = (TaskBean) session.getAttribute("task");
 		UserBean user = (UserBean) session.getAttribute("user"); */
 		
-		//テスト用のタスクIDとログインユーザー取得のための記述
+		//テスト用にタスクの詳細とログインユーザーIDを取得する
 		String userId = "t-yamada";
 		TaskDAO taskDao = new TaskDAO();
-		TaskBean task = null;
+		TaskBean task = new TaskBean();
 		try {
-			task = taskDao.getTaskDetail(4);
+			task = taskDao.getTaskDetail(8);
 		
-			//コメントが未入力の場合は例外を投げる
 			if (commentText == "") {
+				//コメントが未入力の場合は投稿失敗画面を表示し、例外を投げる
 				RequestDispatcher rd = request.getRequestDispatcher("comment-post-failure.jsp");
 				rd.forward(request, response);
 				throw new IllegalArgumentException();
+			
+			} else if (commentText.length() > 100) {
+				//コメントが規定の文字数以上の場合は投稿失敗画面を表示し、例外を投げる
+				RequestDispatcher rd = request.getRequestDispatcher("comment-post-failure.jsp");
+				rd.forward(request, response);
+				throw new SQLException();
+				
+			} else if (task.getTaskId() == 0 && task.getTaskName() == null 
+						&& task.getCategoryId() == 0 && task.getUserId() == null
+						&& task.getStatusCode() == null) {
+				//対象タスクが別ブラウザで削除された場合は投稿失敗画面を表示し、例外を投げる
+				RequestDispatcher rd = request.getRequestDispatcher("comment-post-failure.jsp");
+				rd.forward(request, response);
+				throw new SQLIntegrityConstraintViolationException();
+				
 			} else {
 				CommentBean comment = new CommentBean();
 				CommentDAO commentDao = new CommentDAO();
 				
-				//コメント登録メソッドを呼び出し、登録件数から登録成功か否かを判別して画面遷移させる
-				int count = 0;
 				comment.setTaskId(task.getTaskId());
-					
-				//本番用のログインユーザーをBeanに詰める記述
-				//comment.setUserId(user.getUserId());
-					
-				//テスト用のログインユーザーをBeanに詰める記述
-				comment.setUserId(userId);
-					
 				comment.setCommentText(commentText);
 					
+				//ログインユーザーIDをBeanに詰める
+				//comment.setUserId(user.getUserId());
+					
+				//テスト用にログインユーザーIDをBeanに詰める
+				comment.setUserId(userId);
+					
+				//コメント登録メソッドを呼び出し、登録件数から登録成功か否かを判別して画面遷移させる
+				int count = 0;
 				count = commentDao.commentPost(comment);
 					
 				if (count > 0) {
