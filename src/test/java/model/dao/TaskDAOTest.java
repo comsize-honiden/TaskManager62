e866ed8ac9f4ccdc542c37cec37dd88ecd9239e2
comsize-watanabe <minori.watanabe@comsize.com>
2026-04-27 15:41:10 +0900

    更新　TaskDAOTest
    テストメソッドの追加

--- src/test/java/model/dao/TaskDAOTest.java
+++ src/test/java/model/dao/TaskDAOTest.java
@@ -58,7 +58,182 @@ class TaskDAOTest {
 		}
 		
 		// Assert
+		assertEquals(0, insertCount);
+	}
+	
+	@Test
+	void testInsertTask_存在しないユーザIDを設定する場合_データベースの登録に失敗する() {
+		
+		// Arrange
+		TaskBean task = new TaskBean();
+		TaskDAO taskDao = new TaskDAO();
+		int insertCount = 0;
+		
+		task.setTaskName("a");
+		task.setCategoryId(1);
+		task.setLimitDate(LocalDate.of(2000, 1, 1));
+		task.setUserId("a");
+		task.setStatusCode("00");
+		task.setMemo("");
+		
+		// Act
+		try {
+			insertCount = taskDao.insertTask(task);
+		} catch (ClassNotFoundException | SQLException e) {
+		}
+		
+		// Assert
+		assertEquals(0, insertCount);
+	}
+
+	@Test
+	void testInsertTask_存在しないカテゴリIDを設定する場合_データベースの登録に失敗する() {
+		
+		// Arrange
+		TaskBean task = new TaskBean();
+		TaskDAO taskDao = new TaskDAO();
+		int insertCount = 0;
+		
+		task.setTaskName("a");
+		task.setCategoryId(10);
+		task.setLimitDate(LocalDate.of(2000, 1, 1));
+		task.setUserId("i-sato");
+		task.setStatusCode("00");
+		task.setMemo("");
+		
+		// Act
+		try {
+			insertCount = taskDao.insertTask(task);
+		} catch (ClassNotFoundException | SQLException e) {
+		}
+		
+		// Assert
+		assertEquals(0, insertCount);
+	}
+	
+	@Test
+	void testInsertTask_存在しないステータスコードを設定する場合_データベースの登録に失敗する() {
+		
+		// Arrange
+		TaskBean task = new TaskBean();
+		TaskDAO taskDao = new TaskDAO();
+		int insertCount = 0;
+		
+		task.setTaskName("a");
+		task.setCategoryId(1);
+		task.setLimitDate(LocalDate.of(2000, 1, 1));
+		task.setUserId("i-sato");
+		task.setStatusCode("01");
+		task.setMemo("");
+		
+		// Act
+		try {
+			insertCount = taskDao.insertTask(task);
+		} catch (ClassNotFoundException | SQLException e) {
+		}
+		
+		// Assert
+		assertEquals(0, insertCount);
+	}
+	
+	@Test
+	void testInsertTask_50文字のタスク名を設定する場合_データベースに登録される() {
+		
+		// Arrange
+		TaskBean task = new TaskBean();
+		TaskDAO taskDao = new TaskDAO();
+		int insertCount = 0;
+		
+		task.setTaskName("12345678901234567890123456789012345678901234567890");
+		task.setCategoryId(1);
+		task.setLimitDate(LocalDate.of(2000, 1, 1));
+		task.setUserId("i-sato");
+		task.setStatusCode("00");
+		task.setMemo("");
+		
+		// Act
+		try {
+			insertCount = taskDao.insertTask(task);
+		} catch (ClassNotFoundException | SQLException e) {
+		}
+		
+		// Assert
 		assertEquals(1, insertCount);
+	}
+	
+	@Test
+	void testInsertTask_51文字のタスク名を設定する場合_データベースの登録に失敗する() {
+		
+		// Arrange
+		TaskBean task = new TaskBean();
+		TaskDAO taskDao = new TaskDAO();
+		int insertCount = 0;
+		
+		task.setTaskName("123456789012345678901234567890123456789012345678901");
+		task.setCategoryId(1);
+		task.setLimitDate(LocalDate.of(2000, 1, 1));
+		task.setUserId("i-sato");
+		task.setStatusCode("00");
+		task.setMemo("");
+		
+		// Act
+		try {
+			insertCount = taskDao.insertTask(task);
+		} catch (ClassNotFoundException | SQLException e) {
+		}
+		
+		// Assert
+		assertEquals(0, insertCount);
+	}
+	
+	@Test
+	void testInsertTask_100文字のメモを設定する場合_データベースに登録される() {
+		
+		// Arrange
+		TaskBean task = new TaskBean();
+		TaskDAO taskDao = new TaskDAO();
+		int insertCount = 0;
+		
+		task.setTaskName("a");
+		task.setCategoryId(1);
+		task.setLimitDate(LocalDate.of(2000, 1, 1));
+		task.setUserId("i-sato");
+		task.setStatusCode("00");
+		task.setMemo("1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890");
+		
+		// Act
+		try {
+			insertCount = taskDao.insertTask(task);
+		} catch (ClassNotFoundException | SQLException e) {
+		}
+		
+		// Assert
+		assertEquals(1, insertCount);
+	}
+	
+	@Test
+	void testInsertTask_101文字のメモを設定する場合_データベースの登録に失敗する() {
+		
+		// Arrange
+		TaskBean task = new TaskBean();
+		TaskDAO taskDao = new TaskDAO();
+		int insertCount = 0;
+		
+		task.setTaskName("a");
+		task.setCategoryId(1);
+		task.setLimitDate(LocalDate.of(2000, 1, 1));
+		task.setUserId("i-sato");
+		task.setStatusCode("00");
+		task.setMemo("12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901");
+		
+		// Act
+		try {
+			insertCount = taskDao.insertTask(task);
+		} catch (ClassNotFoundException | SQLException e) {
+		}
+		
+		// Assert
+		assertEquals(0, insertCount);
 	}
 	
 	@Test
@@ -71,7 +246,7 @@ class TaskDAOTest {
 		
 		task.setTaskName("a");
 		task.setCategoryId(1);
-		task.setLimitDate(LocalDate.of(2000, 1, 1));
+		task.setLimitDate(null);
 		task.setUserId("i-sato");
 		task.setStatusCode("00");
 		task.setMemo("");
