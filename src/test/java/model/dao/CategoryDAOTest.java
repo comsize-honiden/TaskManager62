fc273860662576487b1374263b279c1b4bdad3f6
comsize-yoshizawa <kou.yoshizawa@comsize.com>
2026-04-28 15:27:08 +0900

    追加　CategoryDAOTestクラス

---
+++ src/test/java/model/dao/CategoryDAOTest.java
@@ -0,0 +1,24 @@
+package dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import model.dao.CategoryDAO;
+import model.entity.CategoryBean;
+
+public class CategoryDAOTest {
+
+	@Test
+	void get_category_dao_成功() throws ClassNotFoundException, SQLException {
+		CategoryDAO dao = new CategoryDAO();
+		List<CategoryBean> categoryList = dao.getCategoryList();
+
+		assertNotNull(categoryList);
+		assertEquals(categoryList.get(0).getCategoryId(), 1);
+		assertEquals(categoryList.get(0).getCategoryName(), "新商品A：開発プロジェクト");
+	}
+}
