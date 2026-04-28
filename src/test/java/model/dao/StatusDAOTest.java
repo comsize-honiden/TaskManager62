1a2c9e9813e59469928a32d78f4c6896331a4f64
comsize-yoshizawa <kou.yoshizawa@comsize.com>
2026-04-28 15:27:57 +0900

    追加　StatusDAOTestクラス

---
+++ src/test/java/model/dao/StatusDAOTest.java
@@ -0,0 +1,27 @@
+package dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import model.dao.StatusDAO;
+import model.entity.StatusBean;
+
+public class StatusDAOTest {
+
+	@Test
+	void get_status_成功() throws ClassNotFoundException, SQLException {
+		StatusDAO dao = new StatusDAO();
+		List<StatusBean> status = new ArrayList<>();
+		
+		status = dao.getStatusList();
+		
+		assertNotNull(status);
+		assertEquals(status.get(0).getStatusCode(),"00");
+		assertEquals(status.get(0).getStatusName(),"未着手");
+	}
+}
