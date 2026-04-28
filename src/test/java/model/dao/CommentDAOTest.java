5e49008803cc64e5f8adb26a9b4e890dcc262660
comsize-yamamura <takafumi.yamamura@comsize.com>
2026-04-28 14:54:23 +0900

    追加　テストクラスCommentDAOTest

---
+++ src/test/java/model/dao/CommentDAOTest.java
@@ -0,0 +1,58 @@
+package model.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+class CommentDAOTest {
+////
+	@Test
+	void コメント削除_正常() {
+		
+		//Arrange
+		CommentDAO dao = new CommentDAO();
+		int res = 0;
+		
+		//Act
+		try {
+			
+			res = dao.deleteComment(1);
+		
+		} catch (SQLException | ClassNotFoundException e) {
+			
+			e.printStackTrace();
+			
+		}
+		
+		//Assert
+		assertEquals(1, res,"戻り値が期待値と一致しません");
+	
+	}
+
+	@Test
+	void コメント削除_失敗() {
+		
+		//Arrange
+		CommentDAO dao = new CommentDAO();
+		int res = 1;
+		
+		//Act
+		try {
+			
+			res = dao.deleteComment(0);
+		
+		} catch (SQLException | ClassNotFoundException e) {
+			
+			e.printStackTrace();
+			
+		}
+		
+		//Assert
+		assertEquals(0, res,"戻り値が期待値と一致しません");
+	
+	}
+	
+	
+}
